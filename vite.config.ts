f3d7a3f4fd5955c4e6e863ae9ace5e0b0e82eb52
simonostergaard-maker <simon.ostergaard@obsidianagency.com>
2025-10-20 09:24:08 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -15,9 +15,9 @@ export default defineConfig(({ mode }) => {
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
       },
       resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
+  alias: {
+    '@': path.resolve(__dirname, './src'),
+  }
+}
     };
 });
